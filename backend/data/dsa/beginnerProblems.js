ccdacf5ccab82c106646a9807fa5c4ebce869c9c
siddartharavula <siddartharavula2005@gmail.com>
2026-09-22 18:41:37 +0530

    Fix beginner DSA platform logos

--- backend/data/dsa/beginnerProblems.js
+++ backend/data/dsa/beginnerProblems.js
@@ -1,27 +1,31 @@
 const beginnerProblems = [
   {
     topic: "Arrays",
-    platform: "geeksforgeeks",
     problems: [
       {
         name: "Largest Element in an Array",
         link: "https://www.geeksforgeeks.org/problems/largest-element-in-array4009/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Second Largest Element in an Array",
         link: "https://www.geeksforgeeks.org/problems/second-largest3735/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Reverse an Array",
         link: "https://www.geeksforgeeks.org/problems/reverse-an-array/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Sum of Odd and Even Elements",
         link: "https://www.geeksforgeeks.org/problems/sum-of-odd-and-even-elements3033/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Missing Number in Array",
         link: "https://www.geeksforgeeks.org/problems/missing-number-in-array1416/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Find Duplicate Number in Array",
@@ -31,10 +35,12 @@ const beginnerProblems = [
       {
         name: "First and Last Occurrences of X",
         link: "https://www.geeksforgeeks.org/problems/first-and-last-occurrences-of-x3116/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Find Unique Element",
         link: "https://www.geeksforgeeks.org/problems/find-unique-element2632/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Max Altitude",
@@ -66,19 +72,21 @@ const beginnerProblems = [
 
   {
     topic: "Matrix",
-    platform: "geeksforgeeks",
     problems: [
       {
         name: "Row Sum in a Matrix",
         link: "https://www.geeksforgeeks.org/problems/row-sum-in-a-matrix/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Column Sum in a Matrix",
         link: "https://www.geeksforgeeks.org/problems/column-sum-in-a-matrix/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Addition of Two Square Matrices",
         link: "https://www.geeksforgeeks.org/problems/addition-of-two-square-matrices4916/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Transpose Matrix",
@@ -88,25 +96,28 @@ const beginnerProblems = [
       {
         name: "Multiply 2 Matrices",
         link: "https://www.geeksforgeeks.org/problems/multiply-2-matrices4144/1",
+        platform: "geeksforgeeks",
       },
     ],
   },
 
   {
     topic: "Strings",
-    platform: "geeksforgeeks",
     problems: [
       {
         name: "Reverse a String",
         link: "https://www.geeksforgeeks.org/problems/reverse-a-string/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Palindrome String",
         link: "https://www.geeksforgeeks.org/problems/palindrome-string0817/1",
+        platform: "geeksforgeeks",
       },
       {
         name: "Anagram",
         link: "https://www.geeksforgeeks.org/problems/anagram-1587115620/1",
+        platform: "geeksforgeeks",
       },
     ],
   },
